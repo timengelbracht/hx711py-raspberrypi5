75c200fd96a3606fca2253712e5bce693e3af934
Kempei Igarashi <kempe@iga-network.com>
2022-03-24 22:39:02 +0900

    add logzero

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='hx711',
     version='0.1',
     description='HX711 Python Library for Jetson Nano',
-    py_modules=['hx711']
+    py_modules=['hx711'],
+    install_requires=['logzero']
 )
