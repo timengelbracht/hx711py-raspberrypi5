a0708cee7b0189c813f36113d0427a391228136b
Kempei Igarashi <kempe@iga-network.com>
2022-03-24 22:30:28 +0900

    for Jetson Nano

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 setup(
     name='hx711',
     version='0.1',
-    description='HX711 Python Library for Raspberry Pi',
-    py_modules=['hx711'],
-    install_requires=['Rpi.GPIO', 'numpy'],
+    description='HX711 Python Library for Jetson Nano',
+    py_modules=['hx711']
 )
-
